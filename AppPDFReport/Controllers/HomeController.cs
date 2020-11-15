dd72a595b2778c840b8aeb07f48d29d3c57c6bdc
Juan Carlos <jcfmalaga@gmail.com>
2020-11-15 01:32:08 +0100

    Add project files.

---
+++ AppPDFReport/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using AppPDFReport.Models;
+using Microsoft.AspNetCore.Mvc;
+using Wkhtmltopdf.NetCore;
+
+namespace AppPDFReport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        readonly IGeneratePdf _generatePdf;
+
+        public HomeController(IGeneratePdf generatePdf)
+        {
+            _generatePdf = generatePdf;
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> GetEmployeeInfo() 
+        {
+            var empObj = new EmployeeInfo
+            {
+                EmpId = "1001",
+                EmpName = "Mr. Robert",
+                Departament = "IT",
+                Designation = "Software Engineer"
+            };
+
+            return await _generatePdf.GetPdf("Views/Employee/EmployeeInfo.cshtml", empObj);
+        }
+    }
+}
---
+++ AppPDFReport/Models/EmployeeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppPDFReport.Models
+{
+    public class EmployeeInfo
+    {
+        public string EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string Departament { get; set; }
+
+        public string Designation { get; set; }
+
+    }
+}
